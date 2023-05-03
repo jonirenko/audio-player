78f95f4931c7a11f51783238889083eeb60b1282
jonirenko <93704954+jonirenko@users.noreply.github.com>
2023-05-03 12:24:52 +0300

    Upload to github

---
+++ src/data/tracks.js
@@ -0,0 +1,22 @@
+// audio files
+import bane from './Bane of My Existence.mp3';
+import castaway from './Castaway.mp3';
+
+// thumbnails
+import bane_thumb from './Bane of my existence_small.jpg';
+import castaway_thumb from './Castaway_small.jpg';
+
+export const tracks = [
+    {
+        title: 'Bane of My Existence',
+        src: bane,
+        author: 'Children of the Frost',
+        thumbnail: bane_thumb,
+    },
+    {
+        title: 'Castaway',
+        src: castaway,
+        author: 'Children of the Frost',
+        thumbnail: castaway_thumb,
+    },
+]
